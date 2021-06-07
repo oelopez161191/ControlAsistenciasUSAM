1b1ade1b7ed5e93993fa670269a2946450524bd8
Oscar Lopez <oelsuarez91@gmail.com>
2021-06-06 18:46:11 -0600

    CRUD Operaciones
    Se realiza carga de crud operaciones

---
+++ MVC_SEGURIDAD/Models/CRUD/OPERACIONESvista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SEGURIDAD.Models.CRUD
+{
+    public class OPERACIONESvista
+    {
+        public int CODOPERA { get; set; }
+
+        public string NOMOPERA { get; set; }
+
+        public int? CODMOD { get; set; }
+    }
+}
