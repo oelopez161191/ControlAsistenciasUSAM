f0af5aa42c66f79dd6e73e850737c8dd432ea98c
alecenteno <acentg4595@gmail.com>
2021-06-08 22:38:17 -0600

    Actualizacion diagrama Seguridad implementando Control de Asistencia

--- MVC_SEGURIDAD/Models/CRUD/BDSEGURIDAD.Designer.cs
+++ MVC_SEGURIDAD/Models/CRUD/BDSEGURIDAD.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Source_Home\documents\visual studio 2015\Projects\MVC_SEGURIDAD\MVC_SEGURIDAD\Models\CRUD\BDSEGURIDAD.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\documentos\progra3\ProyectoAsistencias\SEGURIDAD_ASISTENCIA\MVC_SEGURIDAD\Models\CRUD\BDSEGURIDAD.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
