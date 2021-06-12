a1efec3f9ab048916b5ba5590638113f61ec3cdc
Luis Lopez <lelsuarez2015@gmail.com>
2021-06-12 13:16:43 -0600

    Revert "Auto stash before merge of "Visual" and "origin/Visual""
    This reverts commit cdf2e5e82450a17c3d046bf8a6a6a88b9dd4d650.

--- MVC_SEGURIDAD/Controllers/HttPostAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MVC_SEGURIDAD.Controllers
-{
-    internal class HttPostAttribute : Attribute
-    {
-    }
-}
--- MVC_SEGURIDAD/Controllers/MateriasController.cs
+++ MVC_SEGURIDAD/Controllers/MateriasController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 //importar clases que se usan en el proyecto
 using MVC_SEGURIDAD.Filters;
 using MVC_SEGURIDAD.Models.CRUD;
+using MVC_SEGURIDAD.Models;
 
 namespace MVC_SEGURIDAD.Controllers
 {
