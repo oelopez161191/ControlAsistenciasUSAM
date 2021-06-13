5101c2f398a107a03d0625a6932d71810d796bb3
Luis Lopez <lelsuarez2015@gmail.com>
2021-06-12 19:56:10 -0600

    ultimos  cambios

---
+++ MVC_SEGURIDAD/Models/CRUD/HORARIOMATERIAcrud.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SEGURIDAD.Models.CRUD
+{
+
+    public class HORARIOMATERIAcrudInsert 
+    {
+        public string idcodigo_horariomateria { get; set; }
+        public int horario { get; set; }
+        public string materia { get; set; }
+        public string docente { get; set; }
+    }
+
+    public class HORARIOMATERIAcrudUpdate
+    {
+        public string idcodigo_horariomateria { get; set; }
+        public int horario { get; set; }
+        public string materia { get; set; }
+        public string docente { get; set; }
+        public object[] id_Horariocontroller { get; internal set; }
+    }
+}
---
+++ MVC_SEGURIDAD/Models/CRUD/HORARIOMATERIAvista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_SEGURIDAD.Models.CRUD
+{
+    public class HORARIOMATERIAvista
+    {
+        public string codigo_horariomateria { get; set; }
+        public int horario { get; set; }
+        public string materia { get; set; }
+        public string docente { get; set; }
+    }
+}
